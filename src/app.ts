41bb2eb0ffa683870b5b9b68e063952f0f408820
habibchy947 <habibullahalquaderi2005@gmail.com>
2025-12-06 00:45:41 -0600

    create table db

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import initDB from "./config/db";
 
 const app = express()
 
+// initializingDB
+initDB()
 
 app.use(express.json())
 
--- src/config/db.ts
+++ src/config/db.ts
@@ -5,3 +5,44 @@ export const pool = new Pool({
     connectionString: config.connectionStr
 })
 
+const initDB = async () => {
+    // table of users
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL CHECK (email=lower(email)),
+        password VARCHAR(200) NOT NULL CHECK (length(password)>=6),
+        phone VARCHAR(20) NOT NULL,
+        role VARCHAR(50) CHECK(role IN ('admin', 'customer')) DEFAULT 'customer'
+        )
+        `);
+
+    // table of vehicles 
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(100) NOT NULL,
+        type VARCHAR(20) CHECK(type IN ('car', 'bike', 'van', 'SUV')),
+        registration_number VARCHAR(50) UNIQUE NOT NULL,
+        daily_rent_price NUMERIC(10,2) NOT NULL CHECK(daily_rent_price > 0),
+        availability_status VARCHAR(20) CHECK (availability_status IN ('available', 'booked')) DEFAULT 'available'
+        )
+        `)
+
+    // table of Bookings
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS bookings(
+        id SERIAL PRIMARY KEY,
+        customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL CHECK (rent_start_date >= CURRENT_DATE),
+        rent_end_date DATE NOT NULL CHECK (rent_end_date > rent_start_date),
+        total_price NUMERIC(10,2) NOT NULL CHECK(total_price > 0),
+        status VARCHAR(20) CHECK (status IN ('active', 'cancelled', 'returned')) DEFAULT 'active'
+        )
+        `)
+    
+}
+
+export default initDB;
